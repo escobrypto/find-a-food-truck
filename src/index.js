da5dc7a6747b5c6838fcb251387a9bd652dffabc
escobrypto <colinjoseph2001@gmail.com>
2026-02-20 13:45:09 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-1. Click **"Add file"** → **"Create new file"**
-2. In the filename box, type exactly: `src/index.js`
-3. Paste the contents below
-4. Click **"Commit changes"**
-
-```js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -14,6 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-```
-
----
